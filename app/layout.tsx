5153fc537b8e0d611518b2fec8bb0442e22720f5
Angel castañeda <luis.castaneda@noktos.com>
2025-07-16 14:37:16 -0600

    Probando pruebas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { environment } from "@/lib/constants";
 import "./globals.css";
 import { Providers } from "@/context/providers";
 
@@ -14,6 +15,11 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className="bg-gradient-to-br from-sky-200 to-sky-400 h-screen">
+        {!!environment && (
+          <div className="fixed top-0 w-[20vw] left-0 text-center text-xl font-bold text-white bg-red-700 z-[900]">
+            {environment.toUpperCase()}
+          </div>
+        )}
         <Providers>{children}</Providers>
       </body>
     </html>
