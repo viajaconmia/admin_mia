3eb6f624e86d0eec6724d9b70ef9f3e829cc6a95
Angel castañeda <luis.castaneda@noktos.com>
2025-08-08 11:10:51 -0600

    fix(reservas/client) - REPLICAR RESERVAS TABLE EN CLIENT Y RESERVAS

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -27,13 +27,13 @@ import Modal from "@/components/organism/Modal";
 import NavContainer from "@/components/organism/NavContainer";
 import { AgentDetailsCard } from "./_components/DetailsClient";
 import { UsersClient } from "./_components/UsersClient";
-import { PageReservasClientes } from "@/components/template/PageReservaClient";
+import PageReservasClientes from "@/components/template/PageReservaClient";
 import PageCuentasPorCobrar from "@/components/template/PageCuentasPorCobrar";
 import { ToolTip } from "@/components/atom/ToolTip";
 import { set } from "date-fns";
 
 function App() {
-  const [clients, setClient] = useState<(Agente)[]>([]);
+  const [clients, setClient] = useState<Agente[]>([]);
   const [selectedItem, setSelectedItem] = useState<Agente | null>(null);
   const [searchTerm, setSearchTerm] = useState<string | null>("");
   const [loading, setLoading] = useState(false);
@@ -49,7 +49,6 @@ function App() {
         item.nombre_agente_completo.toUpperCase().includes(searchTerm) ||
         item.id_agente.toUpperCase().replaceAll("-", "").includes(searchTerm)
       // item.correo.to
-
     )
     .map((item) => ({
       creado: item.created_at,
@@ -192,7 +191,9 @@ function App() {
       component: (
         <PageCuentasPorCobrar
           agente={selectedItem}
-          walletAmount={selectedItem?.wallet ? parseFloat(selectedItem.wallet) : 0}
+          walletAmount={
+            selectedItem?.wallet ? parseFloat(selectedItem.wallet) : 0
+          }
         />
       ),
     },
