bdffb86d98764cd747bb0583af3068b3f7e4dae2
emiliano <desarrollo.mia@noktos.com>
2025-09-05 13:38:35 -0600

    ha quedo

--- app/dashboard/facturacion/_components/reservations-main.tsx
+++ app/dashboard/facturacion/_components/reservations-main.tsx
@@ -307,12 +307,14 @@ const FacturacionModal: React.FC<{
     Serie: null,
 
     Folio: Math.round(Math.random() * 999999999),
-    PaymentForm: "",
-    PaymentMethod: "PUE",
+    PaymentForm: selectedPaymentForm,
+    PaymentMethod: selectedPaymentMethod,
     Exportation: "01",
     Items: [] as any[],
   });
-
+  useEffect(() => {
+    console.log("SELECTEDPAYMENTMETHOD", selectedPaymentMethod)
+  }, [selectedPaymentMethod])
   // Preparar los datos de las reservaciones con sus items seleccionados
   useEffect(() => {
     if (reservations.length > 0 && Object.keys(selectedItems).length > 0) {
@@ -544,7 +546,7 @@ const FacturacionModal: React.FC<{
     reservationsWithSelectedItems,
     isConsolidated,
   ]);
-  console.log(cfdi);
+  console.log(cfdi, "feeeeeeeeeeeeeeeeeeeee");
   const validateInvoiceData = () => {
     if (reservationsWithSelectedItems.length === 0) {
       alert("No hay items seleccionados para facturar");
