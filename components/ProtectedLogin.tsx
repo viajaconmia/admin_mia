d9522de49339270fe6e157c42fb14664622d7aaf
Angel Castañeda <luisacast.29@gmail.com>
2025-11-01 12:35:38 -0600

    done

--- components/ProtectedLogin.tsx
+++ components/ProtectedLogin.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useAuth } from "@/context/AuthContext";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Loader } from "./atom/Loader";
+import { useParams } from "wouter";
 
 export default function ProtectedRoute({
   children,
@@ -12,7 +13,11 @@ export default function ProtectedRoute({
   // 1. Obtenemos tanto `isAuthenticated` como el crucial estado `loading`.
   const { isAuthenticated, loading, user } = useAuth();
   const router = useRouter();
+  const path = usePathname();
 
+  if (path.includes("/secure-payment")) {
+    return <>{children}</>;
+  }
   if (loading) {
     return (
       <div className="w-full h-full flex justify-center items-center">
