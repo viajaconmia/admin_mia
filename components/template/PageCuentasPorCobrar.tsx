4088e0238a155d4459abc445768e5be02643efb4
emilianoruiz-boop <emiliano.ruiz@noktos.com>
2025-11-03 09:03:13 -0600

    Cambios de apagar wallet

--- components/template/PageCuentasPorCobrar.tsx
+++ components/template/PageCuentasPorCobrar.tsx
@@ -634,6 +634,7 @@ const PageCuentasPorCobrar: React.FC<PageCuentasPorCobrarProps> = ({
       comentario: saldo.notas || saldo.comentario || null,
       facturable: saldo.is_facturable ? "Si" : "No",
       comprobante: saldo.comprobante || null,
+      is_cancelado: saldo.is_cancelado,
       acciones: { row: saldo },
       item: saldo,
     }));
@@ -1199,7 +1200,6 @@ const PageCuentasPorCobrar: React.FC<PageCuentasPorCobrarProps> = ({
             is_descuento: item.is_descuento,
             link_stripe: item.link_stripe || null,
             tipo_tarjeta: item.tipo_tarjeta,
-            activo: false, // Cambiamos a 0 para desactivar
             comentario: item.comentario || null,
             comprobante: item.comprobante,
             currency: item.currency || "MXN",
