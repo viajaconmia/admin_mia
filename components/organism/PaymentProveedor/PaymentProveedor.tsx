c0f77f4654c1017ce32fca150f3a86738b52b420
Neftaly Juarez <gersonneftaly1@gmail.com>
2026-06-19 13:23:55 -0600

    se colocaron os filtros en el front de conciliacion

--- components/organism/PaymentProveedor/PaymentProveedor.tsx
+++ components/organism/PaymentProveedor/PaymentProveedor.tsx
@@ -1601,7 +1601,8 @@ export const PaymentModal = ({ reservation, onClose }: Props) => {
                 type="button"
                 onClick={handleSendCoupon}
                 className="bg-blue-600 hover:bg-blue-700"
-                disabled={isSubmitting}
+                // disabled={isSubmitting}
+                disabled
               >
                 <Send className="mr-2 h-4 w-4" />
                 Enviar Cupón (PDF)
